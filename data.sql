e6a4c7d1d1010e17a251c50e5dc4eac18d549fc6
narasimhareddyprostack <narasimhareddyk18@gmail.com>
2023-08-22 11:04:09 +0530

    leaning sql

---
+++ data.sql
@@ -0,0 +1,15 @@
+insert into employees values(101,'rahul',45000,50,'wayanad','male');
+insert into employees values(102,'sonia',45000,70,'New Delhi','female');
+insert into employees values(103,'priyanka',45000,40,'Bangalore','female');
+insert into employees values(104,'modi',45000,80,'Bangalore','male');
+insert into employees values(105,'dk',45000,60,'wayanad','male');
+insert into employees values(106,'disha patani',145000,20,'wayanad','female');
+insert into employees values(107,'deekshith',45000,19,'New Delhi','male');
+insert into employees values(108,'shiva',45000,21,'Bangalore','male');
+insert into employees values(109,'vamsi',45000,21,'wayanad','male');
+insert into employees values(110,'vignesh',85000,22,'New Delhi','male');
+insert into employees values(111,'viswa',95000,22,'Bangalore','male');
+insert into employees values(112,'sai',35000,23,'wayanad','male');
+insert into employees values(113,'supraja',55000,20,'New Delhi','female');
+insert into employees values(114,'vit',65000,20,'Bangalore','female');
+insert into employees values(115,'anjali',75000,20,'wayanad','female');
